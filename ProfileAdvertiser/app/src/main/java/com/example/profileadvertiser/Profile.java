7821d1f190bedfa348b8e05c2d7443ba4b49d7cd
99002584 <girisha.kv@ltts.com>
2020-12-04 10:12:15 +0530

    profileof advertiser

---
+++ ProfileAdvertiser/app/src/main/java/com/example/profileadvertiser/Profile.java
@@ -0,0 +1,96 @@
+package com.example.profileadvertiser;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.profileadvertiser.R;
+import com.example.profileadvertiser.customAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Profile#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Profile extends Fragment {
+    ArrayList<String> title= new ArrayList<>(Arrays.asList("WoodEssence","Good Looks","Homemakers Furniture","WoodBling","Heavenly Clothes"));
+    ArrayList<String> description= new ArrayList<>(Arrays.asList("Shop the luxury furniture you need","Fashion friendly clothes for fashion enthusiasts",
+            "\n" +
+            "Decorate your home with Furniture ","Crafted with excellence","We provide clothes more than just stitched fabric"));
+    ArrayList<String> category= new ArrayList<>(Arrays.asList("Furniture","Clothing","Furniture","Furniture","Clothing"));
+    ArrayList<String> endDate= new ArrayList<>(Arrays.asList("Running","Past","Running","Past","Coming soon"));
+    Integer[] ar={R.drawable.fur1,R.drawable.cloth4,R.drawable.fur4,R.drawable.fur3,R.drawable.cloth2};
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Profile() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Profile.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Profile newInstance(String param1, String param2) {
+        Profile fragment = new Profile();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView campaignList=view.findViewById(R.id.recyclerView);
+
+        LinearLayoutManager linearLayoutManager= new LinearLayoutManager((getContext()));
+        linearLayoutManager.setAutoMeasureEnabled(true);
+        campaignList.setLayoutManager(linearLayoutManager);
+        campaignList.setNestedScrollingEnabled(false);
+        customAdapter customeAdapter =new customAdapter(title,description,category,endDate,ar,getContext());
+        campaignList.setAdapter(customeAdapter);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
---
+++ ProfileAdvertiser/app/src/main/java/com/example/profileadvertiser/customAdapter.java
@@ -0,0 +1,114 @@
+package com.example.profileadvertiser;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+
+public class customAdapter extends RecyclerView.Adapter<customAdapter.MyViewHolder>{
+
+        ArrayList<String> cTitle;
+        ArrayList<String> cDescription;
+        ArrayList<String> cCategory;
+        ArrayList<String> cStatus;
+        Integer[] arr;
+        Context context;
+
+        public customAdapter(ArrayList<String> cTitle, ArrayList<String> cDescription, ArrayList<String> cCategory, ArrayList<String> cStatus, Integer[] arr, Context context) {
+            this.cTitle = cTitle;
+            this.cDescription = cDescription;
+            this.cCategory = cCategory;
+            this.cStatus = cStatus;
+            this.arr = arr;
+            this.context = context;
+        }
+
+        @NonNull
+        @Override
+        public customAdapter .MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.card,parent,false);
+            MyViewHolder vh= new MyViewHolder(v);
+            return vh;
+        }
+
+
+    //
+        @Override
+        public void onBindViewHolder(@NonNull customAdapter.MyViewHolder holder, int position) {
+            TextView htitle=holder.title;
+            TextView hdescription=holder.description;
+            TextView hcat=holder.cat;
+            TextView hdate=holder.sts;
+            Button hknwMore=holder.knwMore;
+            ImageView himage=holder.image;
+            CardView cam_card=holder.card;
+
+            cam_card.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) v.getContext();
+                    Fragment campaign=new campaign();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame, campaign).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).addToBackStack("Campagin ID").commit();
+                }
+            });
+            hknwMore.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) v.getContext();
+                    Fragment campaign=new campaign();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame, campaign).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).addToBackStack("Campagin ID").commit();
+                }
+            });
+
+            htitle.setText(cTitle.get(position));
+            hdescription.setText(cDescription.get(position));
+            hcat.setText(cCategory.get(position));
+            hdate.setText(cStatus.get(position));
+            himage.setImageResource(arr[position]);
+            //button on click
+            //card on click
+        }
+
+        @Override
+        public int getItemCount() {
+            return cTitle.size();
+
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView title;
+            TextView description;
+            TextView cat;
+            TextView sts;
+            Button knwMore;
+            ImageView image;
+            CardView card;
+
+            public MyViewHolder(View v) {
+                super(v);
+                title=(TextView) v.findViewById(R.id.title);
+                description=(TextView) v.findViewById(R.id.descrpt);
+                cat=(TextView) v.findViewById(R.id.cat);
+                sts=(TextView) v.findViewById(R.id.status);
+                knwMore=(Button) v.findViewById(R.id.knw);
+                image=(ImageView)v.findViewById(R.id.image);
+                card=(CardView)v.findViewById(R.id.card);
+
+            }
+        }
+
+    }
+
